7c662a0cffbb31c2e59212d4cbd139057f08c037
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-10 23:01:29 +0100

    registered PropertyCategory in admin and changed Aptadmin to propertyAdmin

--- airbnb_app/admin.py
+++ airbnb_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Apt
+from .models import Property, PropertyCategory
 
 # Register your models here.
-class AptAdmin(admin.ModelAdmin):
+class PropertyAdmin(admin.ModelAdmin):
     list_display = ('title', 'location')
+    empty_value_display = '-empty-'
+
+class PropertyCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    empty_value_display = '-empty-'
 
 # Register your models here.
-admin.site.register(Apt, AptAdmin)
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(PropertyCategory, PropertyCategoryAdmin)
