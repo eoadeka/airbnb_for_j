b53dcc18c92336dd62732f2e272793d1e327f43f
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-16 00:06:30 +0100

    removed cityAdmin and HighlightsAdmin and just registered the models

--- airbnb_app/admin.py
+++ airbnb_app/admin.py
@@ -1,15 +1,43 @@
 from django.contrib import admin
-from .models import Property, PropertyCategory
+from .models import *
+from amenities.models import *
 
 # Register your models here.
+# class CityAdmin(admin.ModelAdmin):
+#     list_display = ('city',)
+#     empty_value_display = '-empty-'
+
+# class HighlightsAdmin(admin.ModelAdmin):
+#     list_display = ('highlight',)
+#     empty_value_display = '-empty-'
+
+
+
+
+# PROPERTIES-RELATED ADMINS
+class PropertyImagesAdmin(admin.StackedInline):
+    model = PropertyImages
+    
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'location')
+    list_display = ('title', 'type', 'city')
+    list_filter = ('type', 'city')
+    inlines = [PropertyImagesAdmin]
+    prepopulated_fields= {'slug': ['title']}
     empty_value_display = '-empty-'
+
+class PropertyImagesAdmin(admin.ModelAdmin):
+    list_display = ('property',)
 
 class PropertyCategoryAdmin(admin.ModelAdmin):
     list_display = ('title', )
     empty_value_display = '-empty-'
 
 # Register your models here.
+
+
+
+admin.site.register(City)
+admin.site.register(Highlights)
+admin.site.register(PropertyImages, PropertyImagesAdmin)
 admin.site.register(Property, PropertyAdmin)
 admin.site.register(PropertyCategory, PropertyCategoryAdmin)
