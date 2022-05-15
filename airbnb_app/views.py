50e704e9add2557b0e2d1e6fe4cf8ad424ef6a45
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-16 00:10:26 +0100

    created Properties and PropertyImagesViews

--- airbnb_app/views.py
+++ airbnb_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import PropertySerializer, PropertyImagesSerializer
+from .models import *
+from amenities.models import *
 
 # Create your views here.
 def main(request):
-    return HttpResponse("<h1>Hello There</h1>")
+    return HttpResponse("<h1>Airbnb app</h1>")
+
+class PropertiesView(viewsets.ModelViewSet):
+    serializer_class = PropertySerializer
+    queryset = Property.objects.all()
+
+class PropertyImagesView(viewsets.ModelViewSet):
+    serializer_class = PropertyImagesSerializer
+    queryset = PropertyImages.objects.all()
