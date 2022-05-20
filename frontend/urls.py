97155ed1bb0454ed76446ecf761ae5232fc376d4
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-20 22:38:42 +0100

    added new paths

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,9 +1,16 @@
 
 
-from django.urls import path
+from django.urls import path, re_path
 from .views import index
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', index)
+    # re_path(r".*", index),
+    path('', index),
+    path('about', index),
+    path('cities', index),
+    path('cities/<slug:slug>', index),
+    path('properties', index),
+    path('properties/<str:id>/<slug:slug>', index),
 ]
