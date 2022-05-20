f235a9a91e1277a038ad1d813a1c062d54380eaf
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-20 22:33:15 +0100

    created APIView for City

--- airbnb_app/views.py
+++ airbnb_app/views.py
@@ -1,13 +1,21 @@
+from importlib.resources import contents
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
-from .serializers import PropertySerializer, PropertyImagesSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CitySerializer, PropertySerializer, PropertyImagesSerializer
 from .models import *
 from amenities.models import *
 
 # Create your views here.
 def main(request):
-    return HttpResponse("<h1>Airbnb app</h1>")
+    return HttpResponse("<h1>Airbnb Django app</h1>")
+
+class CitiesView(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
 
 class PropertiesView(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
@@ -16,3 +24,13 @@ class PropertiesView(viewsets.ModelViewSet):
 class PropertyImagesView(viewsets.ModelViewSet):
     serializer_class = PropertyImagesSerializer
     queryset = PropertyImages.objects.all()
+
+class CitiesAPIView(APIView):
+
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, format=None):
+        cities = City.objects.all()
+        serializer = CitySerializer(cities, many=True)
+        return Response(serializer.data)
+
