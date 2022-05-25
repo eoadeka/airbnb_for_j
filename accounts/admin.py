6e527100c461c15f23ce1c8b01c9ca13ad109d1e
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-25 20:23:04 +0100

    registered UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import User
+# from .forms import *
+# from django.contrib.auth import get_user_model
 
+
+# admin.site.unregister(User)
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define admin model for custom User model with no email field."""
+    # add_form = UserCreationForm
+    fieldsets = (
+        (None, {'fields' : ('email', 'password', 'last_login', 'created_at', 'updated_at',)}),
+        ('Personal Info', {'fields': (
+            'firstname',
+            'lastname',
+        )}),
+        ('Permissions', {'fields' : (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','password1','password2'),
+        }),
+    )
+    readonly_fields = ('created_at', 'updated_at',)
+    list_display = ('email', 'get_full_name', 'is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    ordering = ('email',)
+
+# User = get_user_model()
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+
+# from django.contrib.auth.models import Group as DjangoGroup
+# from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+
+
+# class Group(DjangoGroup):
+#     """Instead of trying to get new user under existing `Aunthentication and Authorization`
+#     banner, create a proxy group model under our Accounts app label.
+#     Refer to: https://github.com/tmm/django-username-email/blob/master/cuser/admin.py
+#     """
+
+#     class Meta:
+#         verbose_name = _('group')
+#         verbose_name_plural = _('groups')
+#         proxy = True
+
+
+# admin.site.unregister(DjangoGroup)
+
+
+# @admin.register(Group)
+# class GroupAdmin(BaseGroupAdmin):
+#     pass
