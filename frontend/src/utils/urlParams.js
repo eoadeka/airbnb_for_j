228e5710225dea9a7e7323be3e7721e701fa7e54
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-27 23:13:01 +0100

    moved utils to src/ folder

---
+++ frontend/src/utils/urlParams.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export function withUrlParams(Component) {
+  return (props) => {
+    const params = useParams();
+    return <Component {...props} params={params} />;
+  };
+}
