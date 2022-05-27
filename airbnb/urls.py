155e4ed4fcf463f3c53dfa33ad16b6b0801b0219
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-27 23:18:16 +0100

    added dj-rest-auth urls

--- airbnb/urls.py
+++ airbnb/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from accounts import views
+from accounts.views import *
 from airbnb_app import views
+from django.contrib.auth.views import LogoutView
 
 
 # urlpatterns = router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -31,8 +32,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(('airbnb.routers', 'airbnb'), namespace='airbnb_api')),
     path('', include('frontend.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+
+    # path('accounts/', include('accounts.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
+    path('dj-rest-auth/twitter/', TwitterLogin.as_view(), name='twitter_login'),
+    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('logout', LogoutView.as_view()),
+
+
+
     path('airbnb/', include('airbnb_app.urls')),
     path('amenities/', include('amenities.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
