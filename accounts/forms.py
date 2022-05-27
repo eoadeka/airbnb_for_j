6d6b3fbc9f43be32ff9d32a20973a40278218ae4
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-27 23:14:00 +0100

    created UserRegisterForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,23 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
-class UserCreationForm(forms.ModelForm):
+class UserRegisterForm(UserCreationForm):
+    firstname = forms.CharField(max_length=32, help_text='First Name')
+    lastname = forms.CharField(max_length=32, help_text='Last Name')
+    username = None
+    email = forms.EmailField(required=True, max_length=64, help_text='Enter a valid email address')
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('email','firstname','lastname','password1', 'password2', 'avatar')
+        help_texts = {
+            'username': None,
+            'email': None,            
+        }
+        
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
@@ -17,7 +27,8 @@ class UserCreationForm(forms.ModelForm):
         return password2
 
     def save(self, commit=True):
-        user = super().save(commit=False)
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
