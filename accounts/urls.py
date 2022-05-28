b46a49b61c0041f62fcf0a3285034d57281e3f1e
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-05-28 23:57:15 +0100

    new url

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,23 +16,18 @@ Including another URLconf
 
 from django.urls import path, re_path
 from .views import main
-# from dj_rest_auth.registration.views import RegisterView, VerifyEmailView
-# from dj_rest_auth.views import LoginView, LogoutView
-
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView
+# from rest_framework_simplejwt.views import obtain_jwt_token
 
 
 urlpatterns = [
     path('', main),
     # path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
-    # path('signup/', RegisterView.as_view(), name='register'),
-    # path('login/', LoginView.as_view()),
-    # path('logout/', LogoutView.as_view()),
+    path('signup/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 
-    # path('verify-email/',
-    #      VerifyEmailView.as_view(), name='rest_verify_email'),
-    # path('account-confirm-email/',
-    #      VerifyEmailView.as_view(), name='account_email_verification_sent'),
-    # re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
-    #      VerifyEmailView.as_view(), name='account_confirm_email'),
-    # path('accounts', main),
+
+    path('accounts', main),
 ]
