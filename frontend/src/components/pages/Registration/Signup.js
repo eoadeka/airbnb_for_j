c0233b50730314918420828677119f616f1abe7a
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-06-01 23:58:10 +0100

    trying to get image to upload

--- frontend/src/components/pages/Registration/Signup.js
+++ frontend/src/components/pages/Registration/Signup.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Signup extends Component{
     constructor(props){
         super(props);
-        // this.myRef = React.createRef(null);
+        this.myRef = React.createRef();
         this.state = {
             avatar: null,
             email: '',
@@ -33,7 +33,8 @@ export default class Signup extends Component{
 
     handleChange = (event) => {
         this.setState({ 
-            [event.target.name]: event.type === "file" ? event.target.files[0] : event.target.value,
+            [event.target.name]: event.target.value,
+            // [event.target.name]: event.type === "file" ? event.target.files[0] : event.target.value,
             // email: event.target.value,
             // first_name: event.target.value,
             // last_name: event.target.value,
@@ -41,14 +42,23 @@ export default class Signup extends Component{
             // password1: event.target.value,
             // password2: event.target.value,
         });
-
-
+    }
+    handleImageChange = (event) =>{
+        this.setState({
+            avatar: event.target.files[0] 
+        })
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
+        console.log(this.state.avatar);
         const formData = new FormData();
-        formData.append('file',  this.state.avatar);
+        formData.append('avatar', this.state.avatar, this.state.avatar.name);
+        formData.append('email',  this.state.email);
+        formData.append('first_name',  this.state.first_name);
+        formData.append('last_name',  this.state.last_name);
+        formData.append('password1',  this.state.password1);
+        formData.append('password2',  this.state.password2);
         const user = {
             email: this.state.email,
             first_name: this.state.first_name,
@@ -57,12 +67,16 @@ export default class Signup extends Component{
             password1: this.state.password1,
             password2: this.state.password2
         };
-        console.log(this.state.avatar);
+        // console.log(this.state.avatar);
+        // console.log(this.state.first_name);
+        // console.log(this.state.last_name);
+        // console.log(this.state.avatar);
+        // console.log(this.state.avatar.name);
         // const bodyFormData = new FormData();
         // bodyFormData.append('file', this.myRef.files[0]);
         
         // formData.append('fileName', file.name);
-        formData.append('user', JSON.stringify(user));
+        // formData.append('user', JSON.stringify(user));
         // url to use: http://127.0.0.1:8000/api/v1/users/dj-rest-auth/registration/
         
         // h.Accept = 'application/json'
@@ -71,12 +85,17 @@ export default class Signup extends Component{
             method: 'POST',
             headers: {
                 // Accept: 'application/json'
-                // 'Content-Type': 'application/json'
-                'Content-Type': 'multipart/form-data'
+                // 'Content-Type': 'application/json',
+                // 'Content-Type': 'multipart/form-data'
+                "accept": "application/json",
+                // "Autorization": "Bearer mybearer token",
+                "Content-Type": "multipart/form-data "
+                // 'X-CSRFToken': csrftoken
             },
             body:formData
             })
             .then(res => res.json())
+            .then(json => console.log(json))
             // .then(res => {console.log(res)})
             .then(data => {
                 if (data.key) {
@@ -88,16 +107,16 @@ export default class Signup extends Component{
                         email:'',
                         first_name:'',
                         last_name:'',
-                        avatar: null,
+                        avatar: {},
                         password1:'',
                         password2:'',
                         errors: true
                     })
                     localStorage.clear();
                 }
-                console.log(user)
-                console.log(user.avatar)
-                console.log(user.avatar.name)
+                // console.log(user)
+                // console.log(user.avatar)
+                // console.log(user.avatar.name)
                 // console.log(formData)
             })
             .catch((err) => console.log(err))
@@ -116,7 +135,7 @@ export default class Signup extends Component{
             <div>
                 {loading === false && <h1>Signup</h1>}
                 {errors === true && <h2>Cannot signup with provided credentials</h2>}
-                <form onSubmit={this.handleSubmit} method="POST" >
+                <form onSubmit={this.handleSubmit}>
                     <label htmlFor="email">Email</label><br></br>
                     <input type="email" value={email} onChange={this.handleChange} name="email" required></input>
                     <br></br>
@@ -143,7 +162,8 @@ export default class Signup extends Component{
                     <br></br>
 
                     <label htmlFor="avatar">Profile Picture</label><br></br>
-                    <input type="file"  onChange={this.handleChange} name="avatar"></input>
+                    <input type="file" ref={this.myRef} id="avatar" accept="image/*" onChange={this.handleImageChange} name="avatar"></input>
+                    {/* <input type="file"  id="avatar" accept="image/png, image/jpeg" onChange={this.handleImageChange} name="avatar"></input> */}
                     {/* <input type="file"  value={avatar} onChange={this.handleChange} name="avatar"></input> */}
                     <br></br>
                     <br></br>
