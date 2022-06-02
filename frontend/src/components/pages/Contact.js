b2d3427e179dbd3727f7fcf1d70718480221de48
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-06-02 23:42:45 +0100

    Contact page

---
+++ frontend/src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    
+    render(){
+        return(
+            <div>
+                <h2>Contact Us</h2>
+                <p>email us at: <a href="mailto:abc123@gmail.com?subject=Enquiry">abc123@gmail.com</a></p>
+                <p>OR call us at: <a href="tel:+2349012345678">+234-901-234-5678</a></p>
+            </div>
+        )
+    }
+}
