4b9bf80154a95acaf500d3c67580c16c60371d37
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-06-08 19:43:21 +0100

    created functions to handle change and submit

--- frontend/src/components/pages/Registration/Password/PasswordChange.js
+++ frontend/src/components/pages/Registration/Password/PasswordChange.js
@@ -6,36 +6,103 @@ export default class PasswordChange extends Component{
         this.state = {
             email: '',
             isAuth: true,
+            errors: false,
+            password: '',
+            old_password: '',
+            new_password1: '',
+            new_password2: '',
         }
+        // this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount(){
         if (localStorage.getItem('token') === null) {
             this.setState({
-                isAuth: false
+                isAuth: false,
+                
             })
             window.location.replace('http://127.0.0.1:8000/login');
+        } else {
+            fetch('http://127.0.0.1:8000/api/v1/users/dj-rest-auth/user/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Token ${localStorage.getItem('token')}`
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    password: data.password,
+                    loading: false
+                })
+            });
         }
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]  : event.target.value
+        })
+    }
 
-    // url: api/v1/users/ dj-rest-auth/ password/change/ 
-    
-    render(){
-        const { isAuth } = this.state;
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        const passwords = {
+            old_password: this.state.old_password,
+            new_password1: this.state.new_password1,
+            new_password2: this.state.new_password2
+        };
+
+        fetch('http://127.0.0.1:8000/api/v1/users/dj-rest-auth/password/change/', {
+        // fetch('http://127.0.0.1:8000/api/token/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Token ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify(passwords)
+            })
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                if (this.state.password !== this.state.old_password && this.state.old_password !== this.state.new_password1 || this.state.old_password !== this.state.new_password2) {
+                    localStorage.clear();
+                    window.location.replace('http://127.0.0.1:8000/login');
+                } else {
+                    this.setState({
+                        old_password: '',
+                        new_password1: '',
+                        new_password2: '',
+                        errors: true
+                    })
+                }
+            })
+            .catch((err) => console.log(err))
+        }
+        
+        
+        // url: api/v1/users/ dj-rest-auth/ password/change/ 
+        
+        render(){
+        // console.log(this.state.password)
+        const { old_password, new_password1, new_password2, errors, isAuth } = this.state;
 
         return(
             <div>
                 { isAuth === true && (
                     <Fragment>
                         <h2>Change Your Password</h2>
+                        { errors === true && <h2>Invalid password</h2> }
                         <br></br>
-                        <form>
-                            <input type="password" name="old_password" placeholder="Old Password"></input>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="password" value={old_password} onChange={this.handleChange} name="old_password" placeholder="Old Password"></input>
                             <br></br>
-                            <input type="password" name="new_password1" placeholder="New Password"></input>
+                            <input type="password" value={new_password1} onChange={this.handleChange} name="new_password1" placeholder="New Password"></input>
                             <br></br>
-                            <input type="password" name="new_password2" placeholder="Confirm New Password"></input>
+                            <input type="password" value={new_password2} onChange={this.handleChange} name="new_password2" placeholder="Confirm New Password"></input>
                             <br></br>
                             <br></br>
                             <button type="submit">Change Password</button>
