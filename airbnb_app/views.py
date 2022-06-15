ee96941b92ea0ee670b34b6ec2b508af8e472434
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-06-15 23:26:19 +0100

    created BookingsView

--- airbnb_app/views.py
+++ airbnb_app/views.py
@@ -5,13 +5,17 @@ from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CitySerializer, PropertySerializer, PropertyImagesSerializer
+from .serializers import BookingSerializer, CitySerializer, PropertySerializer, PropertyImagesSerializer
 from .models import *
 from amenities.models import *
 
 # Create your views here.
 def main(request):
     return HttpResponse("<h1>Airbnb Django app</h1>")
+
+class BookingsView(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
 
 class CitiesView(viewsets.ModelViewSet):
     serializer_class = CitySerializer
